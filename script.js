89d195dd96b08717d1b2b7ec83f79038e9450313
pborkar667 <pranavborkar776@gmail.com>
2025-01-14 19:01:32 -0500

    Add files via upload
    Initial upload of website files

---
+++ script.js
@@ -0,0 +1,31 @@
+// function myMenuFuction() {
+//     var menu8th = document.getElementById("myNavMenu");
+
+//     if (menu8th.className == "nav-menu"){
+//         menu8th.className +="responsive";
+//     }
+//     else{
+//         menu8th.className="nav-menu";
+//     }
+// }
+
+// /*----Dark Mode---*/
+
+// const body = document.querySelector("body"), 
+// toggleSwitch =document.getElementById("toggle-switch");
+
+// toggleSwitch.addEventListener("click", ()=> {
+//     body.classList.toggle("dark");
+// });
+
+// /*----Type Effect---*/
+// var typingEffect = new Typed(".typedText", {
+//     strings:["Designer", "Developer", "Coder"],
+
+
+//     loop:true,
+//     typeSpeed: 100,
+//     backSpeed: 80,
+//     backDeley: 2000,
+// })
+
